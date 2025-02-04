6507dcea6b148c5ead69c6b1703a98e35121623a
hugo <3351279386@qq.com>
2025-02-04 18:53:20 +0800

    Fix the function in temper prac02 with SRP rules. Print number in main function.

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -12,9 +12,13 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            celsius_to_fahrenheit()
+            celsius = float(input("Celsius: "))
+            fahrenheit = celsius_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
         elif choice == "F":
-            fahrenheit_to_celsius()
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
         else:
             print("Invalid option")
         print(MENU)
@@ -22,14 +26,12 @@ def main():
     print("Thank you.")
 
 
-def celsius_to_fahrenheit():
-    celsius = float(input("Celsius: "))
+def celsius_to_fahrenheit(celsius):
     fahrenheit = celsius * 9.0 / 5 + 32
-    print(f"Result: {fahrenheit:.2f} F")
+    return fahrenheit
 
-def fahrenheit_to_celsius():
-    fahrenheit = float(input("Fahrenheit: "))
+def fahrenheit_to_celsius(fahrenheit):
     celsius = 5 / 9 * (fahrenheit - 32)
-    print(f"Result: {celsius:.2f} C")
+    return celsius
 
 main()
