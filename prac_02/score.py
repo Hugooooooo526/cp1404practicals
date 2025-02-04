e4b001341333a0040ee9936b8c516263ccac358b
hugo <3351279386@qq.com>
2025-02-04 18:30:11 +0800

    Refactor the return result function and print result in main function

--- prac_02/score.py
+++ prac_02/score.py
@@ -5,13 +5,19 @@ Broken program to determine score status
 
 # if elif else
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")  
-else:
-    print("Excellent")
+def main():
+    score = float(input("Enter score: "))
+    result = return_result(score)
+    print(result)
 
+def return_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+main()
