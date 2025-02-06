ec72c39c55547e48c0ad307cddb02c795879f3f6
hugo <3351279386@qq.com>
2025-02-06 15:36:02 +0800

    add the random score code in the main function

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,11 +3,17 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# if elif else
+import random
 
 def main():
     score = float(input("Enter score: "))
     result = return_result(score)
+    print(result)
+
+    random_score = random.randint(0,100)
+    print(random_score)
+
+    result = return_result(random_score)
     print(result)
 
 def return_result(score):
