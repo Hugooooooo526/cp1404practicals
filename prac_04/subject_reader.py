5c7ac2d2132248e4dcf1549f85fc23c0ef77b458
Cui Langxuan <serafinmihsugh@gmail.com>
2025-02-22 20:14:54 +0800

    Remove unnessary comment in sub reader file

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,21 +7,6 @@ import os
 
 FILENAME = os.path.join(os.path.dirname(__file__), "subject_data.txt")
 
-"""
-Things to do:
-Copy the starter code and data file from subject_reader.py and subject_data.txt
-Remember to commit these before modifying them any further.
-
-The code reads the file data and processes it into the parts variable, but we want the load_data function to return the data as a list of lists (nested list), like:
-[['CP1401', 'Ada Lovelace', 192],['CP1404', 'Alan Turing', 98]]
-
-Currently, main just prints data. Add a new function to display subject details that produces something like the following:
- output
-
-CP1401 is taught by Ada Lovelace and has 192 students
-CP1404 is taught by Alan Turing  and has  98 students
-"""
-
 def main():
     data = load_data()
     print(data)
