2be9e8affb7c7846a7858ec032ea17915a1c9d05
Cui Langxuan <serafinmihsugh@gmail.com>
2025-03-01 00:41:34 +0800

    Add en comment for hex colour file

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,13 +1,14 @@
 """
-根据上面的州名示例程序，创建一个程序，让你可以查找十六进制颜色代码，如 http://www.color-hex.com/color-names.html上的代码
+Create a program that allows you to look up hexadecimal color codes, such as those found at http://www.color-hex.com/color-names.html.
 
-使用一个包含大约 10 个颜色名称的常量字典并编写一个程序，允许用户输入名称并获取代码，例如输入 AliceBlue（或aliceblue- 使其与大小写无关）应该显示#f0f8ff。
+Using a constant dictionary containing approximately 10 color names, write a program that allows the user to input a name and get the code, for example, inputting AliceBlue (or aliceblue - making it case-insensitive) should display #f0f8ff.
 
-输入无效的颜色名称不应导致程序崩溃。
-允许用户输入名称，直到输入空白名称以停止循环。
+Entering an invalid color name should not cause the program to crash.
+Allow the user to input names until an empty name is entered to stop the loop.
 
 """
 
+# Using a constant dictionary containing approximately 10 color names
 hex_colours = {
     "aliceblue": "#f0f8ff",
     "antiquewhite": "#faebd7",
@@ -21,6 +22,7 @@ hex_colours = {
     "blue": "#0000ff",
 }
 
+# Writing a program that allows the user to input a name and get the code
 colour_name = input("Enter colour name: ").lower()
 while colour_name != "":
     if colour_name in hex_colours:
