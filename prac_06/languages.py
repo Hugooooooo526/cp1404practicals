bc32e8bf4a785e596de4288c9af33a55214cfb4f
hugo <3351279386@qq.com>
2025-03-11 11:20:43 +0800

    Paste programming language raw file

---
+++ prac_06/languages.py
@@ -0,0 +1,18 @@
+# Dynamically import Car class
+import os
+import sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+from programming_language import ProgrammingLanguage
+
+def main():
+    """Demo test code to show how to use car class."""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+    
+    
+    
+    
